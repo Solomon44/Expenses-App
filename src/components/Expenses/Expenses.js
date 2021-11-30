c063a3f24f481f90239d6ea2be7966adabc71058
Solomon Melaku <selomon44@gmail.com>
2021-11-30 10:16:59 +0200

    added ChartExpenses component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,26 +5,28 @@ import ExpensesList from './ExpensesList';
 
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ChartExpenses from './ChartExpenses';
 import './Expenses.css';
 
-const Expenses = (props) => {
+const Expenses = props => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
-  const filterChangeHandler = (selectedYear) => {
+  const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
 
   const filteredExpenses = props.items.filter(
-    (filter) => filter.date.getFullYear() === +filteredYear
+    filter => filter.date.getFullYear() === +filteredYear
   );
 
   return (
     <div>
-      <Card className='expenses'>
+      <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ChartExpenses expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
